dd1a69ba3abd56bd691fc0db270ec8c5bbd50bdc
Seascouter <seascouterdev@icloud.com>
2022-11-08 16:01:46 -0500

    Completed Day1

---
+++ day1/part2.py
@@ -0,0 +1,23 @@
+# Advent of Code 2020
+
+# Input Getter
+with open("input.txt") as f:
+    progInput = [line.rstrip() for line in f]
+
+# Variables
+inputInt = []
+
+
+# Functions
+
+
+
+# Main Code
+for i in progInput:
+    inputInt.append(int(i))
+
+for i in inputInt:
+    for j in inputInt:
+        for k in inputInt:
+            if i + j + k == 2020:
+                print(i*j*k)
